3cb2a3b401863b985bef7139e5902ae24c1d336c
rafimd422 <rafimd2222@gmail.com>
2023-12-21 23:57:29 +0600

    complated the footer section

--- components/shared/Footer.tsx
+++ components/shared/Footer.tsx
@@ -1,9 +1,21 @@
+import Image from "next/image"
+import Link from "next/link"
+import imgFooter from '@/public/assets/images/logo.svg'
 
 const Footer = () => {
   return (
-    <div>
-      Footer
-    </div>
+<footer className="border-t">
+<div className="flex-center wrapper flex-between flex-col flex gap-4 p-5 text-center sm:flex-row">
+  <Link href={'/'}>
+    <Image
+    src={imgFooter}
+    width={128}
+    height={38}
+    alt="logo"
+    />
+  </Link>
+</div>
+</footer>
   )
 }
 
