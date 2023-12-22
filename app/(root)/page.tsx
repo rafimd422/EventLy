8cc195bbd5051cf61b313555e16fed771647c359
rafimd422 <rafimd2222@gmail.com>
2023-12-22 23:33:57 +0600

    created basic structure of event section before adding our events from our database

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -31,6 +31,19 @@ className="max-h-[70vh] object-contain object-center 2xl:max-h-[50vh]"
 />
 </div>
   </section>
+
+<section id="events" className="wrapper my-8 flex flex-col gap-8 md:gap-12">
+<h2 className="h2-bold">
+Trusted by <br /> Thousends of Events
+</h2>
+<div className="flex w-full flex-col gap-5 md:flex-row">
+  Search CategoryFilter
+
+</div>
+
+
+</section>
+
 </>
   )
 }
