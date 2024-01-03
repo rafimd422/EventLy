c893d278f6ebb6ee30aaf6151c21afb932e7003a
rafimd422 <rafimd2222@gmail.com>
2024-01-03 15:38:27 +0600

    created new file on the root file which is called create. here i will write codes for create events section

---
+++ app/(root)/events/create/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const CreateEvent = () => {
+  return (
+<section className='bg-primary-50 bg-dotted-pattern bg-cover bg-center py-5 md:py-10'>
+  
+</section>
+  )
+}
+
+export default CreateEvent
